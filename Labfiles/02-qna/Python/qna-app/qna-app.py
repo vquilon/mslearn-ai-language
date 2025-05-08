67e6265d9fec547128979efaa9f101dbea8616e4
vquilon <v.quilonr@gmail.com>
2025-05-08 02:39:00 -0700

    QNA lab

--- Labfiles/02-qna/Python/qna-app/qna-app.py
+++ Labfiles/02-qna/Python/qna-app/qna-app.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 import os
 
 # Import namespaces
-
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.language.questionanswering import QuestionAnsweringClient
 
 def main():
     try:
@@ -14,10 +15,20 @@ def main():
         ai_deployment_name = os.getenv('QA_DEPLOYMENT_NAME')
 
         # Create client using endpoint and key
-
+        credential = AzureKeyCredential(ai_key)
+        ai_client = QuestionAnsweringClient(endpoint=ai_endpoint, credential=credential)
 
         # Submit a question and display the answer
-
+        user_question = ''
+        while user_question.lower() != 'quit':
+            user_question = input('\nQuestion:\n')
+            response = ai_client.get_answers(question=user_question,
+                                            project_name=ai_project_name,
+                                            deployment_name=ai_deployment_name)
+            for candidate in response.answers:
+                print(candidate.answer)
+                print("Confidence: {}".format(candidate.confidence))
+                print("Source: {}".format(candidate.source))
 
 
     except Exception as ex:
@@ -25,4 +36,5 @@ def main():
 
 
 if __name__ == "__main__":
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
     main()
--- Labfiles/02-qna/ask-question.sh
+++ Labfiles/02-qna/ask-question.sh
@@ -1,6 +1,6 @@
 
-prediction_url="YOUR_ENDPOINT_URL"
-key="YOUR_KEY"
+prediction_url="https://ai-ls-vqr-qna.cognitiveservices.azure.com/"
+key="5spR1Gi9VIWkMpAyrjaudgROneena0e5fTe09nCqyew9aihjMumvJQQJ99BEACYeBjFXJ3w3AAAaACOGdE4P"
 
 curl -X POST $prediction_url -H "Ocp-Apim-Subscription-Key: $key" -H "Content-Type: application/json" -d "{'question': 'What is a learning Path?' }"
 
